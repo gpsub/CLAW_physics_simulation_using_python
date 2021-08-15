a6b1a925d41cf82c12a1f2cba453057934c850eb
gpsub <giriprasad590@gmail.com>
2021-08-15 15:30:01 +0530

    made function to return rewards

--- mod_code/cv2_code/main.py
+++ mod_code/cv2_code/main.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 from time import time
+
+from pygame import Rect
 from .windowcapture import WindowCapture
 
 ## TODO: return the rewards and the xy coordinates from the bounding boxes
@@ -11,13 +13,13 @@ class window_det():
         self.wincap = WindowCapture('CLAW_Arm')
         loop_time = time() # for fps
         self.reward = 0
-    def iou(self,area1,area2):
-        x1,y1,w1,h1 = area1
-        x2,y2,w2,h2= area2
-        if(x1>=x2) and (x1<=(x2+w2)) and (y1>=y2) and (y1<=y2+w2):
-            return 1
-
-    
+    def iou(self,rect1,rect2):
+        x = 0
+        ((x1,y1),(w1,h1),(a1)) = rect1
+        ((x2,y2),(w2,h2),(a2)) = rect2
+        if (x1>=x2-w2)and (x1<=x2+w2) and (y1>=y2-h2) and (y1<=y2+h2):
+             x =1
+        return x      
     def ret_reward(self):
         # get an updated image of the game
         screenshot = self.wincap.get_screenshot()
@@ -25,28 +27,31 @@ class window_det():
         ret,thresh = cv.threshold(grey,220,255,cv.THRESH_BINARY_INV)
         
         contours,_ = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-        area1 = []
-        area2 = []
+        
         for c in contours:
             x, y, w, h = cv.boundingRect(c)
             if (w*h==600):
                 continue
-            print("Area:"+str(w*h))
             if w*h == 420:
-                area1 = [x,y,w,h]
+                rect1 = cv.minAreaRect(c)
+                box = cv.boxPoints(rect1)
+                # convert all coordinates floating point values to int
+                box = np.int0(box)
+                # draw a blue rectangle
+                cv.drawContours(screenshot, [box], 0, (255, 0, 0),2)
+       
             else:
-                area2 = [x,w,y,h]
+                
+                rect2 = cv.minAreaRect(c)
+                box = cv.boxPoints(rect2)
+                # convert all coordinates floating point values to int
+                box = np.int0(box)
+                # draw a blue rectangle
+                cv.drawContours(screenshot, [box], 0, (255, 0, 0),2)
+       
 
-            # get the min area rect
-            
-            rect = cv.minAreaRect(c)
-            box = cv.boxPoints(rect)
-            # convert all coordinates floating point values to int
-            box = np.int0(box)
-            # draw a blue rectangle
-            cv.drawContours(screenshot, [box], 0, (255, 0, 0),2)
         try:
-            reward = self.iou(area1,area2)
+            reward = self.iou(rect1,rect2)
         except:
             print("one or more boxes not detected")
             cv.imshow('Detection', screenshot)
